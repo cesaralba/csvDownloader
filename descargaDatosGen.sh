cbbb6a9e234c44bf06ce728dfcb26178ec92126e
cap@CASA <cesar.alba@gmail.com>
2020-06-12 08:43:33 +0200

    Reemplaza el diff de ficheros (inmanejable) por una estadistica

--- descargaDatosGen.sh
+++ descargaDatosGen.sh
@@ -24,17 +24,23 @@ source ${ENVFILE}
 [ -n "${WRKDIR}" ] || adiosMundoCruel "No se ha especificado la variable WRKDIR"
 [ -n "${NEWFILE}" ] || adiosMundoCruel "No se ha especificado la variable NEWFILE"
 [ -n "${DATAFILE}" ] || adiosMundoCruel "No se ha especificado la variable DATAFILE"
-[ -n "${URLFILE}" ] || adiosMundoCruel "No se ha especificado la variable URLFILE"
+[ -n "${URLFILE}" ] || [ -n "${SRCFILE}" ] || adiosMundoCruel "Se necesita especificar o la variable URLFILE o la variable SRCFILE"
 
 DOCOMMIT=0
 
-MSG="Fecha:${DATE} fuente ${URLFILE}"
 
 [ -d ${WRKDIR} ] || mkdir -p ${WRKDIR} || adiosMundoCruel "Problemas creando ${WRKDIR}. Bye"
 
 [ -f ${NEWFILE} ] || rm -f ${NEWFILE} || adiosMundoCruel "Problemas borrando ${NEWFILE}. Bye"
 
-wget -q -O ${NEWFILE} ${URLFILE} || adiosMundoCruel "Problemas descargando ${URLFILE}. Bye"
+if [ -n "${URLFILE}" ]
+then
+  MSG="Fecha:${DATE} fuente ${URLFILE}"
+  wget -q -O ${NEWFILE} ${URLFILE} || adiosMundoCruel "Problemas descargando ${URLFILE}. Bye"
+else
+  MSG="Fecha:${DATE} fuente ${SRCFILE}"
+  cp -f ${SRCFILE} ${NEWFILE} || adiosMundoCruel "Problemas copiando ${SRCFILE}. Bye"
+fi
 
 if [ -f ${DATAFILE} ]
 then
@@ -43,8 +49,9 @@ then
   if [ ${RES} != 0 ]
   then
     echo "Descarga: ${MSG}"
-    diff ${NEWFILE} ${DATAFILE}
+    #diff ${NEWFILE} ${DATAFILE}
     cp ${NEWFILE} ${DATAFILE} || adiosMundoCruel "Problemas copiando de ${NEWFILE} a  ${DATAFILE}. Bye"
+    git diff --shortstat
     (cd $DATADIR ; git add ${DATAFILE} || adiosMundoCruel "No puedo añadir ${DATAFILE} a repo. Bye")
     DOCOMMIT=1
   fi
