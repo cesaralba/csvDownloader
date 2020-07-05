198aa5207a1eb48e2193380f4732651761f1e489
cap@CASA <cesar.alba@gmail.com>
2020-07-05 10:57:20 +0200

    Recuperado master a push

--- descargaDatosGen.sh
+++ descargaDatosGen.sh
@@ -70,7 +70,7 @@ then
   RES=$?
   if [ $RES = 0 ]
   then
-    (cd $DATADIR ; git remote -v ; git push -q origin || adiosMundoCruel "No puedo hacer push a remoto $(git remote -v | grep origin ). Bye")
+    (cd $DATADIR ; git remote -v ; git push -q origin master || adiosMundoCruel "No puedo hacer push a remoto $(git remote -v | grep origin ). Bye")
   fi
 fi
 
