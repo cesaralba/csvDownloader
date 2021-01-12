e3b9c9fc22cc9c9052102dc5b581ad9339c14627
cap@CASA <cesar.alba@gmail.com>
2021-01-12 11:39:36 +0100

    * lib/miscDataFrames.py: DFVersionado2DFmerged: arreglados los contadores de cambios (que comprobaban después de hacer los cambios) * lib/miscDataFrames.py: DFVersionado2DFmerged: añadido parámetro minDate * lib/miscDataFrames.py: DFVersionado2DFmerged: añadido parámetro kwargs que se pasan a la función de lectura * lib/miscDataFrames.py: DFVersionado2DFmerged: documentada la función

--- gitTimeSeries/lib/miscDataFrames.py
+++ gitTimeSeries/lib/miscDataFrames.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from time import time
 
 import pandas as pd
 
-from gitTimeSeries.lib.GitIterator import GitIterator, fileFromCommit
+from .GitIterator import GitIterator, fileFromCommit
 
 
 def compareDataFrameIndexes(new: pd.DataFrame, old: pd.DataFrame = None):
@@ -48,17 +49,43 @@ def leeCSVdataset(fname_or_handle, colIndex=None, cols2drop=None, **kwargs) -> p
 
 
 def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent: pd.DataFrame = None,
-                          changeCounters: dict = None):
-    fechaUltimaActualizacion = DFcurrent['fechaCommit'].max() if DFcurrent is not None else None
+                          minDate: datetime = None, changeCounters: dict = None, **kwargs):
+    """
+    Atraviesa un repositorio git, leyendo las versiones de un fichero susceptible de ser leído con pandas y hace
+    anotaciones de los cambios y las adiciones (cuántos, en qué versión se ha hecho la última modificación).
+    Opcionalmente puede contar cambios en subconjuntos de columnas
+    :param repoPath: directorio raíz del repositorio git
+    :param filePath: ubicación DENTRO DEL REPOSITORIO del fichero a estudiar
+    :param readFunction: función de lectura del fichero a estudiar. Debe aceptar un nombre de fichero o un handle y
+                         devuelve un dataframe de pandas
+    :param DFcurrent: (Opcional) dataframe del estado actual. Sirve para hacer una lectura incremental. Sería el
+                      resultado de la ejecución anterior de la función. Si no se suministra se consideran los datos
+                      procedente de la primera versión como todos nuevos
+    :param minDate: (Opcional) Fecha mínima del commit de la primera versión que se va a procesar.
+    :param changeCounters: (Opcional). Configuración de contadores adicionales de cambios. Espera un diccionario con
+                           pares {nombreContador:[lista de columnas a comparar]}. Añade columnas nombreContador al DF
+                           resultado.
+    :param kwargs: (Opcional) Parámetros adicionales a la función de lectura
+    :return: Dataframe con el último valor leído para cada valor del índice. Tiene las siguientes características:
+            * Índice: mísmo que el devuelto por la función de lectura
+            * Columnas del DF devuelto: por la función de lectura
+            * Columnas adicionales (metadata del último cambio referido al repo que lo contiene)
+            * Contadores de cambios: general y si se han indicado contadores específicos
+    """
+    fechaUltimaActualizacion = None
+    if minDate:
+        fechaUltimaActualizacion = minDate
+    elif DFcurrent is not None:  # Hecha la comprobación de is not None porque pandas se queja
+        fechaUltimaActualizacion = DFcurrent['fechaCommit'].max()
+
     repoIterator = GitIterator(repoPath=repoPath, reverse=True, minDate=fechaUltimaActualizacion)
 
     for commit in repoIterator:
         timeStart = time()
         commitSHA = commit.hexsha
         commitDate = commit.committed_datetime
-        print(commitSHA, commitDate)
 
-        newDF = readFunction(fileFromCommit(filePath, commit))
+        newDF = readFunction(fileFromCommit(filePath, commit), **kwargs)
 
         _, changed, added = compareDataFrames(newDF, DFcurrent)
 
@@ -85,10 +112,6 @@ def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent:
                 continue  # No hay cambiadas porque no hay viejas. Son todas nuevas
 
         if len(changed):
-            DFcurrent.loc[changed, newDF.columns] = newDF.loc[changed, :]
-            DFcurrent.loc[changed, 'shaCommit'] = commitSHA
-            DFcurrent.loc[changed, 'fechaCommit'] = commitDate
-            DFcurrent.loc[changed, 'contCambios'] += 1
             if changeCounters:
                 for counterName, counterCols in changeCounters.items():
                     missingCols = set(counterCols).difference(newDF.columns)
@@ -97,9 +120,14 @@ def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent:
                             f"DFVersionado2DFmerged: {counterName}: columnas desconocidas: {sorted(missingCols)}. ",
                             f"Columnas existentes: {sorted(list(newDF.columns))}. Ignorando contador.")
                         continue
-                    areRowsDifferent = (DFcurrent.loc[changed, counterCols] != newDF.loc[changed, counterCols]).any(axis=1)
+                    areRowsDifferent = (DFcurrent.loc[changed, counterCols] != newDF.loc[changed, counterCols]).any(
+                        axis=1)
                     DFcurrent.loc[changed, counterName] += areRowsDifferent
 
+            DFcurrent.loc[changed, newDF.columns] = newDF.loc[changed, :]
+            DFcurrent.loc[changed, 'shaCommit'] = commitSHA
+            DFcurrent.loc[changed, 'fechaCommit'] = commitDate
+            DFcurrent.loc[changed, 'contCambios'] += 1
 
         if len(added):
             DFcurrent = pd.concat([DFcurrent, newData], axis=0)
