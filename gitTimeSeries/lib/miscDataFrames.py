b6d9a73fa09489deecc798e96128f7b582fb35ed
cap@CASA <cesar.alba@gmail.com>
2021-01-12 10:06:54 +0100

    Movidos módulos a lib, y añadidos contadores parciales

--- gitTimeSeries/lib/miscDataFrames.py
+++ gitTimeSeries/lib/miscDataFrames.py
@@ -2,7 +2,7 @@ from time import time
 
 import pandas as pd
 
-from GitIterator import GitIterator, fileFromCommit
+from gitTimeSeries.lib.GitIterator import GitIterator, fileFromCommit
 
 
 def compareDataFrameIndexes(new: pd.DataFrame, old: pd.DataFrame = None):
@@ -47,7 +47,8 @@ def leeCSVdataset(fname_or_handle, colIndex=None, cols2drop=None, **kwargs) -> p
     return result
 
 
-def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent: pd.DataFrame = None):
+def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent: pd.DataFrame = None,
+                          changeCounters: dict = None):
     fechaUltimaActualizacion = DFcurrent['fechaCommit'].max() if DFcurrent is not None else None
     repoIterator = GitIterator(repoPath=repoPath, reverse=True, minDate=fechaUltimaActualizacion)
 
@@ -66,6 +67,15 @@ def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent:
             newData['shaCommit'] = commitSHA
             newData['fechaCommit'] = pd.to_datetime(commitDate)
             newData['contCambios'] = 0
+            if changeCounters:
+                for counterName, counterCols in changeCounters.items():
+                    missingCols = set(counterCols).difference(newDF.columns)
+                    if missingCols:
+                        print(
+                            f"DFVersionado2DFmerged: {counterName}: columnas desconocidas: {sorted(missingCols)}. ",
+                            f"Columnas existentes: {sorted(list(newDF.columns))}. Ignorando contador.")
+                        continue
+                    newData[counterName] = 0
 
             if DFcurrent is None:
                 DFcurrent = newData
@@ -79,6 +89,17 @@ def DFVersionado2DFmerged(repoPath: str, filePath: str, readFunction, DFcurrent:
             DFcurrent.loc[changed, 'shaCommit'] = commitSHA
             DFcurrent.loc[changed, 'fechaCommit'] = commitDate
             DFcurrent.loc[changed, 'contCambios'] += 1
+            if changeCounters:
+                for counterName, counterCols in changeCounters.items():
+                    missingCols = set(counterCols).difference(newDF.columns)
+                    if missingCols:
+                        print(
+                            f"DFVersionado2DFmerged: {counterName}: columnas desconocidas: {sorted(missingCols)}. ",
+                            f"Columnas existentes: {sorted(list(newDF.columns))}. Ignorando contador.")
+                        continue
+                    areRowsDifferent = (DFcurrent.loc[changed, counterCols] != newDF.loc[changed, counterCols]).any(axis=1)
+                    DFcurrent.loc[changed, counterName] += areRowsDifferent
+
 
         if len(added):
             DFcurrent = pd.concat([DFcurrent, newData], axis=0)
