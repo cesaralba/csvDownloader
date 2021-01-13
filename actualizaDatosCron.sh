b775ebe8dbd4aace02538d2e1af11b984925ba63
cap@CASA <cesar.alba@gmail.com>
2021-01-13 11:27:10 +0100

    * actualizaDatosCron.sh: fichero para descarga desde cron (descarga el código desde GH) y lanza una lista de entornos a descargar * sysconfigSAMPLE: fichero de ejemplo que debe ir a /etc/sysconfig/GitTimeSeries

---
+++ actualizaDatosCron.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+CONFIGFILE=/etc/sysconfig/GitTimeSeries
+
+ENVIRONMENTLIST=$1
+
+function adiosMundoCruel {
+  MSG=${1:-No msg}
+  echo ${MSG}
+  exit 1
+}
+
+[ -n "${ENVIRONMENTLIST}" ] || adiosMundoCruel "No se ha especificado el parámetro con la lista de ficheros de entorno a descargar"
+[ -f "${ENVIRONMENTLIST}" ] || adiosMundoCruel "Fichero con lista de entornos '${ENVIRONMENTLIST}' no existe"
+
+
+[ -f "${CONFIGFILE}" ] || adiosMundoCruel "Fichero con entorno '${CONFIGFILE}' no existe"
+
+source ${CONFIGFILE}
+
+[ -f "${CONFIGFILE}" ] || adiosMundoCruel "Fichero con entorno '${CONFIGFILE}' no existe"
+
+
+[ -n "${GTS_REPO}" ] || adiosMundoCruel "No se ha especificado la variable GTS_REPO"
+[ -n "${GTS_CODEDIR}" ] || adiosMundoCruel "No se ha especificado la variable GTS_CODEDIR"
+
+BASEDIR=$(cd "$(dirname $(readlink -e $0))/../" && pwd )
+TODAY=$(date '+%Y%m%d%H%M')
+
+[ -d ${GTS_CODEDIR} ] && rm -rf  ${GTS_CODEDIR}
+mkdir -p ${GTS_CODEDIR}
+git clone -q --branch master ${GTS_REPO} ${GTS_CODEDIR}
+
+
+for ENTORNO in $(grep -v '^#' ${ENVIRONMENTLIST})
+do
+  ${GTS_CODEDIR}/descargaDatosGen.sh ${ENTORNO}
+done
